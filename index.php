3784506f58d65b6eab9ebf726535de3343f23245
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-11-08 11:20:40 -0600

    change: Change files .php to .html

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     
-    <?php include 'templates/header.php'; ?>
+    <?php include 'templates/header.html'; ?>
    
 </head>
 <body>
 
-    <?php include 'templates/layouts/proximamente.php'; ?>
+    <?php include 'templates/layouts/proximamente.html'; ?>
 
 
         
